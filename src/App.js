99011176228b3b5fe1f63f73ad05993907ca4e28
AndriyKornytskiy <andriy.kornytskiy@gmail.com>
2021-05-01 12:45:03 +0300

    hw3 add to each user clear button

--- src/App.js
+++ src/App.js
@@ -5,56 +5,67 @@ export default function App() {
 
     let usersList = [
         {
+            id: 1,
             name: 'vasya',
             age: 31,
             status: false,
             address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
         }, {
+            id: 2,
             name: 'petya',
             age: 30,
             status: true,
             address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
         }, {
+            id: 3,
             name: 'kolya',
             age: 29,
             status: true,
             address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
         }, {
+            id: 4,
             name: 'olya',
             age: 28,
             status: false,
             address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
         }, {
+            id: 5,
             name: 'max',
             age: 30,
             status: true,
             address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
         }, {
+            id: 6,
             name: 'anya',
             age: 31,
             status: false,
             address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
         }, {
+            id: 7,
             name: 'oleg',
             age: 28,
             status: false,
             address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
         }, {
+            id: 8,
             name: 'andrey',
             age: 29,
             status: true,
             address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
         }, {
+            id: 9,
             name: 'masha',
             age: 30,
             status: true,
             address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
         }, {
+            id: 10,
             name: 'olya',
             age: 31,
             status: false,
             address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
         }, {
+            id: 11,
             name: 'max',
             age: 31,
             status: true,
@@ -65,11 +76,6 @@ export default function App() {
 
     const deleteUser = () => {
         users.pop();
-        setUsers([...users])
-    }
-
-    const clear = () => {
-        users.splice(users.index, 1);
         setUsers([...users])
     }
 
@@ -109,7 +115,8 @@ export default function App() {
                 users.map((value, index) =>
                     <UserComponent key={index}
                                    {...value}
-                                   clearFunc={clear}
+                                   set = {setUsers}
+                                   list = {users}
                     />
                 )
             }
--- src/components/user/user.component.js
+++ src/components/user/user.component.js
@@ -1,13 +1,13 @@
 export default function UserComponent(props) {
 
-    let {address: {city, country, houseNumber, street}, age, name, status, clearFunc} = props;
+    let {address: {city, country, houseNumber, street}, age, id, list, name, set, status, clearFunc} = props;
 
     return (
         <div>
             <h2>{name}</h2>
             <h3>{age} - {status.toString()}</h3>
             <p>{country} {city} {street} {houseNumber}</p>
-            <button onClick={clearFunc}>clear</button>
+            <button onClick={()=> set(list.filter(value=> value.id !== id))}>clear</button>
 
         </div>
     );
