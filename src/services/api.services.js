95edd9bb6839301f7d4bae9cc8923994d6bdcdcc
AndriyKornytskiy <andriy.kornytskiy@gmail.com>
2021-05-08 20:51:53 +0300

    hw3 add axios

---
+++ src/services/api.services.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+let axiosInstance = axios.create({
+   baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
+
+const getUsers = () => {
+    return axiosInstance.get('/users');
+
+};
+
+const getPosts = () => {
+    return axiosInstance.get('/posts');
+};
+
+
+export {
+    getUsers, getPosts, axiosInstance
+};
