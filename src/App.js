9c07df4feb4b769cf87ef16baacefbfab63db20a
AndriyKornytskiy <andriy.kornytskiy@gmail.com>
2021-05-21 23:41:28 +0300

    redux2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const Posts = () => {
+    const {isLoading, posts} = useSelector(({isLoading, posts}) => ({
+        isLoading,
+        posts
+    }));
+    const dispatch = useDispatch();
+
+    const fetchPosts = async () => {
+        try {
+            dispatch({type: 'SET_IS_LOADING'});
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await response.json();
+
+            dispatch({type: 'SET_POSTS', payload: data});
+            dispatch({type: 'RESET_IS_LOADING'});
+            console.log(data);
+        }catch (e) {
+            console.log(e, 'fatal error');
+        }
+    };
+
+
+
+    useEffect(() => {
+        fetchPosts();
+    },[]);
+
+    if (isLoading) {
+        return (
+            <h1>LOADING DATA !!!!</h1>
+        )
+    }
+
+
+    return (
+        <div>
+        {
+            posts.map((post) => (
+                <p key={post.id}>
+                    {post.title} = {post.body}
+                </p>
+            ))
+        }
+        </div>
+    );
+};
+
+
+
+
+export default function App() {
+    return (
+        <div>
+            <Posts/>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/redux/reducers.js
@@ -0,0 +1,30 @@
+const initialState = {
+    posts: [],
+    isLoading: false
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        case 'SET_POSTS': {
+            return {
+                ...state,
+                posts: action.payload
+            }
+        }
+        case 'SET_IS_LOADING': {
+            return {
+                ...state,
+                isLoading: true
+            }
+        }
+        case 'RESET_IS_LOADING': {
+            return {
+                ...state,
+                isLoading: false
+            }
+        }
+        default:
+            return state;
+    }
+}
